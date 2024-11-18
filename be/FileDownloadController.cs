4da9d0ab6ab65911fc632f77fccba1c9d258745b
Sharmila Chandrasekaran <sharmila.chandrasekaran@jeevantechnologies.com>
2024-11-18 18:52:38 +0530

    changes update

--- be/FileDownloadController.cs
+++ be/FileDownloadController.cs
@@ -9,14 +9,14 @@ namespace FileUploadDownloadApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [EnableCors("AllowAll")]  // Allow CORS
+    [EnableCors("AllowAll")] // Enable CORS
     public class FileDownloadController : ControllerBase
     {
         // Endpoint to zip and download the folder
         [HttpGet("download-zip")]
         public IActionResult DownloadZippedFolder()
         {
-            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "crud"); // Specify your folder path
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "crud"); // Path to the crud folder
             string zipFilePath = Path.Combine(Path.GetTempPath(), "crud.zip");  // Temporary path to store the zip file
 
             if (!Directory.Exists(folderPath))
@@ -24,20 +24,25 @@ namespace FileUploadDownloadApi.Controllers
                 return NotFound("The specified folder does not exist.");
             }
 
+            // Create zip file
             if (System.IO.File.Exists(zipFilePath))
             {
-                System.IO.File.Delete(zipFilePath); // Delete existing zip if any
+                System.IO.File.Delete(zipFilePath); // Delete existing zip if it exists
             }
 
-            ZipFile.CreateFromDirectory(folderPath, zipFilePath); // Create zip file from folder
+            ZipFile.CreateFromDirectory(folderPath, zipFilePath); // Create zip from folder
 
-            byte[] fileBytes = System.IO.File.ReadAllBytes(zipFilePath); // Read zip as byte array
-            System.IO.File.Delete(zipFilePath); // Optionally delete the zip file after sending
+            // Read the zip file as bytes
+            byte[] fileBytes = System.IO.File.ReadAllBytes(zipFilePath);
 
-            return File(fileBytes, "application/zip", "Crud.zip");  // Return zip file for download
+            // Optionally delete the zip file after reading it
+            System.IO.File.Delete(zipFilePath);
+
+            // Return the zip file for download
+            return File(fileBytes, "application/zip", "Crud.zip");
         }
 
-        // Endpoint to upload a file
+        // Endpoint to upload files to the crud folder
         [HttpPost("upload")]
         public IActionResult UploadFiles(List<IFormFile> files)
         {
@@ -48,20 +53,20 @@ namespace FileUploadDownloadApi.Controllers
 
             try
             {
-                string uploadFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "crud"); // Path to crud folder
 
-                if (!Directory.Exists(uploadFolderPath))
+                if (!Directory.Exists(folderPath))
                 {
-                    Directory.CreateDirectory(uploadFolderPath);
+                    Directory.CreateDirectory(folderPath); // Create crud folder if it doesn't exist
                 }
 
                 foreach (var file in files)
                 {
-                    var filePath = Path.Combine(uploadFolderPath, file.FileName);
+                    string filePath = Path.Combine(folderPath, file.FileName);
 
                     using (var stream = new FileStream(filePath, FileMode.Create))
                     {
-                        file.CopyTo(stream);
+                        file.CopyTo(stream); // Save the file
                     }
                 }
 
@@ -72,6 +77,5 @@ namespace FileUploadDownloadApi.Controllers
                 return StatusCode(500, "An error occurred while uploading the files: " + ex.Message);
             }
         }
-
     }
 }
